8080010b98880eb1979432579baee41d5394f7a1
Ronald Gil <rsgc85@gmail.com>
2023-09-07 15:45:42 -0600

    Proyecto 1 agregado: matlive.py

---
+++ matlive.py
@@ -0,0 +1,6 @@
+name = input("escriba su nombre por favor: ")
+adj1 = input("agregue una cualidad suya que sea positiva (una palabra): ")
+adj2 = input("agregue una cualidad suya que sea negativa (una palabra): ")
+
+print("Esta es la historia de",name,", por delante es",adj1," y por detras es ",adj2,".")
+
